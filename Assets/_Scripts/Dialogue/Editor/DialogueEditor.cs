6c38f996c192494eedcb38452baca48bfbf6db7f
j32u4ukh <j32u4ukh@gmail.com>
2022-01-17 16:19:26 +0800

    Lesson 8: OnSelectionChange in DialogueEditor

--- Assets/_Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/_Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -8,6 +8,37 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selected_dialogue = null;
+
+        private void OnEnable()
+        {
+            //Selection.selectionChanged += OnSelectionChange;            
+        }
+
+        private void OnSelectionChange()
+        {
+            Dialogue dialogue = Selection.activeObject as Dialogue;
+
+            if (dialogue != null)
+            {
+                selected_dialogue = dialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+            // 參考網站 1: https://docs.unity3d.com/ScriptReference/EditorGUILayout.html
+            if(selected_dialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else
+            {
+                EditorGUILayout.LabelField($"Dialogue({selected_dialogue.name})");
+            }
+        }
+
         // 開啟 Editor 視窗
         [MenuItem("Window/Dialogue Editor")]
         public static void showEditorWindow()
@@ -24,13 +55,13 @@ namespace RPG.Dialogue.Editor
         // 所有 asset 點擊後都會觸發此函式，因此需要利用 instance_id 分辨點擊的對象，是否是我們要處理的 asset，並返回是否可以處理
         // 參考網站 1: https://docs.unity3d.com/ScriptReference/Callbacks.OnOpenAssetAttribute.html
         // 參考網站 2: https://docs.unity3d.com/ScriptReference/EditorUtility.InstanceIDToObject.html
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         public static bool onOpenAsset(int instance_id, int line)
         {
             // 若利用 instance_id 所取得的 Object 實際上不是 Dialogue，則將為 null
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instance_id) as Dialogue;
 
-            if(dialogue != null)
+            if (dialogue != null)
             {
                 //Debug.Log("onOpenDialogue");
                 showEditorWindow();
@@ -38,14 +69,6 @@ namespace RPG.Dialogue.Editor
             }
 
             return false;
-        }
-
-        private void OnGUI()
-        {
-            // 參考網站 1: https://docs.unity3d.com/ScriptReference/EditorGUILayout.html
-            EditorGUILayout.LabelField("Apple");
-            EditorGUILayout.LabelField("Orange");
-            EditorGUILayout.LabelField("Pear");
         }
     }
 }
